75bf023b6c4b5f66eeafe035b83fac8ee2728bdf
Pedro Pereira <1211131@isep.ipp.pt>
2024-10-05 13:44:27 +0100

    Add Java Drools Questions class

---
+++ backend-app-Drools/src/main/java/org/engcia/Utils/Questions.java
@@ -0,0 +1,6 @@
+package org.engcia.Utils;
+
+public class Questions {
+    public static final String ASK_CREATOR_KNOWN = "Is the creator known?";
+    public static final String ASK_CREATOR_RESPONDED = "Has the creator responded?";
+}
