07d926ff2ed84fd4a5d4700e6ad986aba80123eb
Rafael Branco <rafaelbranco16@outlook.pt>
2024-10-17 00:28:10 +0100

    Tentativa fix POST no prolog

--- backend-app-Prolog/service.pl
+++ backend-app-Prolog/service.pl
@@ -7,7 +7,6 @@
 :- use_module(library(http/json_convert)).
 :- use_module(library(http/http_json)).
 :- use_module(library(http/json)).
-:- use_module(library(http/http_cors)).
 :- use_module(library(http/http_open)).
 :- [prolog].
 :- dynamic criador/1.
@@ -17,6 +16,7 @@
 :-http_handler('/api/prolog/hello_world', hello_world, [method(get)]).
 hello_world(Request):-
     http_parameters(Request,[]),
+    cors_enable,
     Response = _{message:"Hello World"},
     atom_json_dict(JSON, Response, []),
     reply_json(JSON).
@@ -41,6 +41,7 @@ test(Request) :-
 
 :- http_handler('/api/prolog/handler', handle_question, [method(post)]).
 handle_question(Request):-
+    cors_enable,
     http_read_json_dict(Request, JSONDict),
     create_fact_from_json(JSONDict, Fact),
     create_dynamic_fact(Fact, _),
@@ -54,11 +55,12 @@ handle_question(Request):-
         facto(N1,_)
     ),
     (
-        facto(N1,questao(Question, Type, PossibleResponses)),
+        facto(N1,questao(Question, Type, PossibleResponses, ParameterNumber)),
         DictOut = _{
             question: Question,
             type: Type,
-            possible_responses: PossibleResponses
+            possible_responses: PossibleResponses,
+            parameter_number: ParameterNumber
         }
         ;
         facto(N1,conclusao(Conlcusao)),
--- frontend-app/src/services/AlertService2.tsx
+++ frontend-app/src/services/AlertService2.tsx
@@ -5,9 +5,9 @@ import axios from 'axios';
 const AlertService = {
   processAlertDrools: async (alertContext: any) => {
     try {
-        console.log("Submitting alert context:", alertContext);
-        const response = await axios.post('http://localhost:8080/api/alerts/process-alert', alertContext);
-        return response.data; // Assuming API sends the next question or conclusion
+      console.log("Submitting alert context:", alertContext);
+      const response = await axios.post('http://localhost:8080/api/alerts/process-alert', alertContext);
+      return response.data; // Assuming API sends the next question or conclusion
     } catch (error) {
       console.error("Error processing alert:", error);
       throw error;
@@ -16,8 +16,21 @@ const AlertService = {
 
   processAlertProlog: async (alertContext: any) => {
     try {
-      const response = await axios.post('http://localhost:8080/api/alerts/process-alert/null', alertContext);
-      return response.data; // Assuming API sends the next question or conclusion
+      const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: alertContext,
+  redirect: "follow" as RequestRedirect
+};
+
+fetch("http://localhost:5000/api/prolog/handler", requestOptions)
+  .then((response) => response.json())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
+
     } catch (error) {
       console.error("Error processing alert:", error);
       throw error;
