89a352f76a93914c17ae5162efe0e13a693f515c
Pedro Pereira <1211131@isep.ipp.pt>
2024-11-20 19:35:17 +0000

    Updated Alert

--- frontend-app/src/domain/Alert.ts
+++ frontend-app/src/domain/Alert.ts
@@ -1,9 +1,14 @@
 interface Alert {
     id: string;
     type: string;
+    subType: string;
     origin: string;
     assignedTo: string;
     status: string;
+    creationTime: string;
+    conclusionTime: string;
+    description: string;
+    resolution: string[];
 }
 
 export default Alert;
