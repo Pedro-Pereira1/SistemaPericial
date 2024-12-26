f5bf25cd4d68f7fff184edda1398064cf0afbfb9
Pedro Pereira <1211131@isep.ipp.pt>
2024-12-26 18:00:50 +0000

    Update alert categorization and model handling with improved error handling

--- backend_ml/src/adapters/ml_adapter.py
+++ backend_ml/src/adapters/ml_adapter.py
@@ -3,9 +3,10 @@ import xgboost as xgb
 
 class MachineLearningAdapter:
     def __init__(self)->None:
-        self.base_model = joblib.load('src/ml/xgb_model.joblib')
+        self.base_model = joblib.load('src/ml/xgboost.joblib')
     
     async def predict(self, model:str, random_line):
+        print("Predict, model:", model)
         dtest = xgb.DMatrix(random_line)
         if not model:
             return self.base_model.get_booster().predict(dtest)        
--- backend_ml/src/router/router.py
+++ backend_ml/src/router/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from src.loaders.loader import loader
 from src import config
 from src.controllers.ml_controller import MachineLearningController
@@ -7,17 +7,25 @@ router = APIRouter()
 
 class Router:
     @router.get("/predict/{model}")
-    async def predict(model:str = None):
-        ml_controller:MachineLearningController = loader.resolve(config.ml_controller["name"]) 
-        return await ml_controller.predict(model) 
+    async def predict(model: str):
+        try:
+            ml_controller: MachineLearningController = loader.resolve(config.ml_controller["name"])
+            return await ml_controller.predict(model)
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
 
     @router.get("/predict")
     async def predict_default():
-        ml_controller:MachineLearningController = loader.resolve(config.ml_controller["name"]) 
-        return await ml_controller.predict(None) 
+        try:
+            ml_controller: MachineLearningController = loader.resolve(config.ml_controller["name"])
+            return await ml_controller.predict(None)
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
     
     @router.get("/genetic")
     async def genetic():
-        ml_controller:MachineLearningController = loader.resolve(config.ml_controller["name"]) 
-        return await ml_controller.genetic() 
-        
+        try:
+            ml_controller: MachineLearningController = loader.resolve(config.ml_controller["name"])
+            return await ml_controller.genetic()
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
